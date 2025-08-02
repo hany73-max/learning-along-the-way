e86aaed552e6d000ecd27420e55acbb11710f9a6
hany73-max <hanyelashry323@gmail.com>
2025-08-02 08:57:24 +0300

    Add WhatsApp messaging functionality and create new Jupyter notebook for tasks 79 to 80

---
+++ programming/data-science/whatapp module.py
@@ -0,0 +1,3 @@
+from pywhatkit import sendwhatmsg, sendwhatmsg_instantly
+
+sendwhatmsg_instantly("+20 15 55973336", "اشتغلت اخيراااااااااا اههعهعهعهعهعههههههعهعهعهع")
---
+++ programming/python elzero course tasks/whatapp module.py
@@ -0,0 +1,3 @@
+from pywhatkit import sendwhatmsg, sendwhatmsg_instantly
+
+sendwhatmsg_instantly("+20 1002564612", "رسالة مسجلة", 5, True, 2)
